edc4ee86064d3c1176135259c10150f080804d85
Arnaud293 <arnaudlt293@gmail.com>
2022-09-21 12:30:00 +0200

    add event on content container to close menu

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 const sidebar = document.getElementById('side-bar');
 const toggleBtn = document.querySelector('.toggle-btn');
+const content = document.querySelector('.content');
 
 toggleBtn.addEventListener('click', () => {
     // sidebar.style.left = 0;
     sidebar.classList.toggle('active');
 } )
+
+content.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+})
 
 
