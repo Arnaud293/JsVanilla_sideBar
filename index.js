96f62cbcfc490284d6919f31939d22947db4d6a7
Arnaud293 <arnaudlt293@gmail.com>
2022-09-21 12:18:25 +0200

    add event + toggle with active className

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
 
-// Ranger la sidebar si on click sur le contenu principal
+const sidebar = document.getElementById('side-bar');
+const toggleBtn = document.querySelector('.toggle-btn');
 
-// BONUS
-// Créer en css l'hamburger menu qui se transforme en croix
+toggleBtn.addEventListener('click', () => {
+    // sidebar.style.left = 0;
+    sidebar.classList.toggle('active');
+} )
 
-// Aide
-// Priorités en CSS : id > classe > baliseHtml
+
